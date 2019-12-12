924a66250fbbff393fc4ea93ee8d2210bbc70fb8
Gildas Cherruel <gildas.cherruel@genesys.com>
2019-12-12 18:30:44 +0900

    Initial code

---
+++ sentinels.go
@@ -0,0 +1,54 @@
+package errors
+
+import (
+	pkerrors "github.com/pkg/errors"
+)
+
+// WithWhat creates a new error from a given sentinal telling "What" is wrong
+func (e *Error) WithWhat(what string) error {
+	final := *e
+	final.What = what
+	return pkerrors.WithStack(&final)
+}
+
+// WithWhatAndValue creates a new error from a given sentinal telling "What" is wrong and the wrong value
+func (e *Error) WithWhatAndValue(what string, value interface{}) error {
+	final := *e
+	final.What  = what
+	final.Value = value
+	return pkerrors.WithStack(&final)
+}
+
+// ArgumentMissingError is used when an argument is missing
+var ArgumentMissingError = &Error{0, "error.argument.missing", "Argument %s is missing", "", nil}
+
+
+// ArgumentInvalidError is used when an argument has an unexpected value
+var ArgumentInvalidError = &Error{0, "error.argument.invalid", "Argument %s is invalid (value: %#v)", "", nil}
+
+// FoundError is used when something is found but it should not have been
+var FoundError = &Error{0, "error.found", "%s %s Found", "", nil}
+
+// JSONMarshalError is used when data failed to be marshaled into JSON
+var JSONMarshalError = &Error{0, "error.json.marshal", "JSON failed to marshal data", "", nil}
+
+// JSONUnmarshalError is used when JSON data is missing a property
+var JSONUnmarshalError = &Error{0, "error.json.unmarshal", "JSON failed to unmarshal data", "", nil}
+
+// JSONPropertyMissingError is used when JSON data is missing a property
+var JSONPropertyMissingError = &Error{0, "error.json.property.missing", "JSON data is missing property %s", "", nil}
+
+// NotFoundError is used when something is not found
+var NotFoundError = &Error{0, "error.notfound", "%s %s Not Found", "", nil}
+
+// NotImplementedError is used when some code/method/func is not written yet
+var NotImplementedError = &Error{0, "error.notimplemented", "%s Not Implemented", "", nil}
+
+// TooManyError is used when something is found too many times
+var TooManyError = &Error{0, "error.toomany", "Too Many", "", nil}
+
+// UnsupportedError is used when something is unsupported by the code
+var UnsupportedError = &Error{0, "error.unsupported", "Unsupported %s: %s", "", nil}
+
+// UnknownError is used when the code does not know which error it is facing...
+var UnknownError = &Error{0, "error.unknown", "Unknown Error: %s", "", nil}
---
+++ types.go
@@ -0,0 +1,53 @@
+package errors
+
+import (
+	"fmt"
+	"strings"
+
+	pkerrors "github.com/pkg/errors"
+)
+
+// Error describes an augmented implementation of Go's error interface
+type Error struct {
+	// Code is an numerical code, like an HTTP Status Code
+	Code int `json:"code"`
+	// ID is the string identifier, like: "error.argument.invalid"
+	ID string `json:"id"`
+	// Text is the human readable error message
+	Text string `json:"text"`
+	// What contains what element is wrong for errors that need it, like NotFoundError
+	What string `json:"what,omitempty"`
+	// Value contains the value that was wrong for errros that need it, like ArgumentInvalidError
+	Value interface{} `json:"value"`
+}
+
+// New returns an error with the supplied message.
+// New also records the stack trace at the point it was called.
+func New(message string) error {
+	return pkerrors.New(message)
+}
+
+func (e *Error) Error() string {
+	// implements error interface
+
+	if i := strings.Index(e.Text, "%s"); i > -1 {
+		if len(e.Text) > i + 2 && strings.Contains(e.Text[i+2:], "%s") { // TODO: detect %v, %d, etc
+			return fmt.Sprintf(e.Text, e.What, e.Value)
+		}
+		return fmt.Sprintf(e.Text, e.What)
+	} 
+	return e.Text
+}
+
+func (e *Error) Format(s fmt.State, verb rune) {
+	// implements fmt.Formatter interface
+}
+
+func (e *Error) Unwrap() error {
+	return pkerrors.Cause(e)
+}
+
+func (e *Error) Is(target error) bool {
+	// TODO: I AM HERE
+	return false
+}
