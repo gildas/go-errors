a49bd952d8c80116f134457947e1cd4dbcb6c6bb
Gildas Cherruel <gildas@breizh.org>
2021-07-27 15:19:39 +0900

    Added some formatters

--- stack-frame.go
+++ stack-frame.go
@@ -2,7 +2,11 @@ package errors
 
 import (
 	"fmt"
+	"io"
+	"path"
 	"runtime"
+	"strconv"
+	"strings"
 )
 
 /*
@@ -48,4 +52,46 @@ func (frame StackFrame) MarshalText() ([]byte, error) {
 		return []byte(funcName), nil
 	}
 	return []byte(fmt.Sprintf("%s %s:%d", funcName, frame.Filepath(), frame.Line())), nil
+}
+
+// Format formats the frame according to the fmt.Formatter interface.
+//
+//    %s    source file
+//    %d    source line
+//    %n    function name
+//    %v    equivalent to %s:%d
+//
+// Format accepts flags that alter the printing of some verbs, as follows:
+//
+//    %+s   function name and path of source file relative to the compile time
+//          GOPATH separated by \n\t (<funcname>\n\t<path>)
+//    %+v   equivalent to %+s:%d
+func (frame StackFrame) Format(s fmt.State, verb rune) {
+	switch verb {
+	case 's':
+		switch {
+		case s.Flag('+'):
+			io.WriteString(s, frame.FuncName())
+			io.WriteString(s, "\n\t")
+			io.WriteString(s, frame.Filepath())
+		default:
+			io.WriteString(s, path.Base(frame.Filepath()))
+		}
+	case 'd':
+		io.WriteString(s, strconv.Itoa(frame.Line()))
+	case 'n':
+		io.WriteString(s, funcname(frame.FuncName()))
+	case 'v':
+		frame.Format(s, 's')
+		io.WriteString(s, ":")
+		frame.Format(s, 'd')
+	}
+}
+
+// funcname removes the path prefix component of a function's name
+func funcname(name string) string {
+	i := strings.LastIndex(name, "/")
+	name = name[i+1:]
+	i = strings.Index(name, ".")
+	return name[i+1:]
 }
--- stack.go
+++ stack.go
@@ -1,7 +1,52 @@
 package errors
 
+import (
+	"fmt"
+	"io"
+)
+
 /*
 Imported from https://github.com/pkg/errors/blob/master/stack.go
 */
 
 type StackTrace []StackFrame
+
+// Format formats the stack of Frames according to the fmt.Formatter interface.
+//
+//    %s	lists source files for each Frame in the stack
+//    %v	lists the source file and line number for each Frame in the stack
+//
+// Format accepts flags that alter the printing of some verbs, as follows:
+//
+//    %+v   Prints filename, function, and line number for each Frame in the stack.
+func (st StackTrace) Format(s fmt.State, verb rune) {
+	switch verb {
+	case 'v':
+		switch {
+		case s.Flag('+'):
+			for _, f := range st {
+				io.WriteString(s, "\n")
+				f.Format(s, verb)
+			}
+		case s.Flag('#'):
+			fmt.Fprintf(s, "%#v", []StackFrame(st))
+		default:
+			st.formatSlice(s, verb)
+		}
+	case 's':
+		st.formatSlice(s, verb)
+	}
+}
+
+// formatSlice will format this StackTrace into the given buffer as a slice of
+// Frame, only valid when called with '%s' or '%v'.
+func (st StackTrace) formatSlice(s fmt.State, verb rune) {
+	io.WriteString(s, "[")
+	for i, f := range st {
+		if i > 0 {
+			io.WriteString(s, " ")
+		}
+		f.Format(s, verb)
+	}
+	io.WriteString(s, "]")
+}
