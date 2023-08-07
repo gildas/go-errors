4178bb83747258ba528045cdc3ea6a0ef59bdddb
Gildas Cherruel <gildas.cherruel@genesys.com>
2023-08-07 12:45:00 +0200

    Issue #2: simplified code

--- error.go
+++ error.go
@@ -63,8 +63,8 @@ func (e Error) Is(target error) bool {
 		}
 		return e.ID == actual.ID
 	}
-	if e.Origin != nil && Is(e.Origin, target) {
-		return true
+	if e.Origin != nil {
+		return Is(e.Origin, target)
 	}
 	return false
 }
